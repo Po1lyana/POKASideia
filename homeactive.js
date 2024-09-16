cabbaed02cd1f2cdc6ba09377d2727daae49b1f9
Po1lyana <pollyana03.cs@gmail.com>
2024-09-16 20:20:36 -0300

    Add files via upload

---
+++ homeactive.js
@@ -0,0 +1,6 @@
+var btExp = document.querySelector('#exp')
+var menuAbrir = document.querySelector('.geral')
+
+btExp.addEventListener('click', function(){
+    menuAbrir.classList.toggle('expandir')
+})
